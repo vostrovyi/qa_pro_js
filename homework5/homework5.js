d5b9b53a7fd5b5a3ff51e0f1b01129e3b120dd8c
Vitalii Ostrovyi <vostrovyi@gmail.com>
2026-03-22 23:07:33 +0200

    updated hw5 version 1.0

--- homework5/homework5.js
+++ homework5/homework5.js
@@ -7,26 +7,22 @@ services['Розбити скло'] = "200 грн";
 Створити метод price(), який обчислює та повертає загальну вартість наданих послуг.
 Створити метод minPrice(), який повертає мінімальну ціну.
 Створити метод maxPrice(), який повертає максимальну ціну.
-Будь ласка, надайте наступні дані для перевірки вашої роботи:
-    */
+*/
 
 var barbar = {
-  стрижка: "60 грн",
-  гоління: "80 грн",
-  "Миття голови": "100 грн",
+  options: {
+    "стрижка": "60 грн",
+    "гоління": "80 грн",
+    "Миття голови": "100 грн",
 
-  teachers: {
-    teacher: "Ivan",
-    mentor: "Oleg",
-  },
-  teachersInfo: function () {
-    console.log(
-      "Main teacher: " +
-        barbar.teachers.teacher +
-        ", mentor: " +
-        barbar.teachers.mentor,
-    );
+    price: function () {
+      let totalPrice = 0;
+
+      //console.log("Main option: " + barbar.options.teacher);
+    },
+    minPrice: function () {},
+    maxPrice: function () {},
   },
 };
 
-barbar.teachersInfo();
+barbar.price();
