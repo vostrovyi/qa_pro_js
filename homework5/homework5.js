34fa1dd22576a16ec5d4385187421f358a040ec4
Vitalii Ostrovyi <vostrovyi@gmail.com>
2026-03-23 22:48:42 +0200

    Added method-helper to valite values

--- homework5/homework5.js
+++ homework5/homework5.js
@@ -7,43 +7,40 @@ services['Розбити скло'] = "200 грн";
 Створити метод maxPrice(), який повертає максимальну ціну.
 */
 
-var barbar = {
-    options: {
-        "стрижка": "60 грн",
-        "гоління": "80 грн",
-        "Миття голови": "100 грн",
-        //'Розбити скло' : "200 грн"
-    },
+var barbarshop = {
+  options: {
+    стрижка: "60 грн",
+    гоління: "80 грн",
+    "Миття голови": "100 грн",
+    //'Розбити скло' : "200 грн"
+  },
 
-    price: function () {
-        let totalPrice = 0;
-        // Array of values: ["60 грн", "80 грн", "100 грн"]
-        let valuesObject = Object.values(this.options);
-        let isValid = true;
+  // Окремий метод для перевірки даних
+  validateValues: function () {
+    let values = Object.values(this.options); // Array of values: ["60 грн", "80 грн", "100 грн"]
+    for (let str of values) {
+      let num = Number.parseInt(str);
 
-        for (let priceStr of valuesObject) {
-            // Перевіряємо, чи є закінчення "грн"
-            if (!priceStr.endsWith("грн")) {
-                isValid = false;
-                break;
-            }
+      // Перевіряємо, чи є закінчення "грн"
+      if (!str.endsWith("грн")) {
+        return false;
+      } else if (!Number.isFinite(num) || !Number.isSafeInteger(num)) {
+        return false;
+      }
+    }
+    return true;
+  },
 
-            // Витягуємо число для перевірки
-            let num = Number.parseInt(priceStr);
+  price: function () {
+    let totalPrice = 0;
+    let valuesObject = Object.values(this.options); // Array of values: ["60 грн", "80 грн", "100 грн"]
 
-            // Перевіряємо, чи число є валідне
-            if (!Number.isFinite(num) || !Number.isSafeInteger(num)) // (false || false) -> повертаємо false, умова не виконується, йдемо далі.
-            {isValid = false;
-                break;}
-        }
+    if (this.validateValues()) {
+      for (let i = 0; i < valuesObject.length; i++) {
+        // Беремо число, ігноруючи " грн"
+        let currentPrice = Number.parseInt(valuesObject[i]);
 
-        if (isValid) {
-
-            for (let i = 0; i < valuesObject.length; i++) {
-                // Беремо число, ігноруючи " грн"
-                let currentPrice = Number.parseInt(valuesObject[i]);
-
-                /* 
+        /* 
                 =========================================================================
                 АБО... через split()
                 // Беремо рядок -> "60 грн"
@@ -57,51 +54,58 @@ var barbar = {
                 =========================================================================
                 */
 
+        //console.log(currentPrice);
+        totalPrice += currentPrice; //Додаємо самого себе
+      }
 
-
-                //console.log(currentPrice);
-                totalPrice += currentPrice; //Додаємо самого себе
-            }
-
-            console.log("Загальна вартість наданих послуг =", totalPrice + " грн");
-            return totalPrice;
-        }
-        else {
-            console.log("Помилка: один з елеменітів не є валідним");
-            return null;
-        }
-    },
-
-    minPrice: function () {
-        let prices = Object.values(this.options);
-        //console.log(prices)
-        let min = Infinity; // Починаємо з нескінченності
-
-        for (let priceStr of prices) {
-            let current = parseInt(priceStr);
-            if (current < min) {
-                min = current; // Оновлюємо мінімум, якщ знайшли менше число
-            }
-        }
-        console.log("Мінімальна ціна =", min + " грн");
-        return min;
-    },
-    maxPrice: function () {
-        let prices = Object.values(this.options);
-        //console.log(prices)
-        let max = -Infinity;
-
-        for (let priceStr of prices) {
-            let current = parseInt(priceStr);
-            if (current > max) {
-                max = current; // Оновлюємо максимум, якщо знайшли більше число
-            }
-        }
-        console.log("Максимальна ціна =", max + " грн");
-        return max;
+      console.log("Загальна вартість наданих послуг =", totalPrice + " грн");
+      return totalPrice;
+    } else {
+      console.log("Помилка: один з елеменітів не є валідним");
+      return null;
     }
+  },
+
+  minPrice: function () {
+    if (this.validateValues()) {
+      let prices = Object.values(this.options);
+      //console.log(prices)
+      let min = Infinity; // Починаємо з нескінченності
+
+      for (let priceStr of prices) {
+        let current = parseInt(priceStr);
+        if (current < min) {
+          min = current; // Оновлюємо мінімум, якщ знайшли менше число
+        }
+      }
+      console.log("Мінімальна ціна =", min + " грн");
+      return min;
+    } else {
+      console.log("Помилка: один з елеменітів не є валідним");
+      return null;
+    }
+  },
+  maxPrice: function () {
+    if (this.validateValues()) {
+      let prices = Object.values(this.options);
+      //console.log(prices)
+      let max = -Infinity;
+
+      for (let priceStr of prices) {
+        let current = parseInt(priceStr);
+        if (current > max) {
+          max = current; // Оновлюємо максимум, якщо знайшли більше число
+        }
+      }
+      console.log("Максимальна ціна =", max + " грн");
+      return max;
+    } else {
+      console.log("Помилка: один з елеменітів не є валідним");
+      return null;
+    }
+  },
 };
 
-barbar.price();
-barbar.minPrice();
-barbar.maxPrice();
+barbarshop.price();
+barbarshop.minPrice();
+barbarshop.maxPrice();
